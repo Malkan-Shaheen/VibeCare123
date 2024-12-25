8f25855efea3f3e1a49ba223febbfd669e308a43
Arooj Raziq <rjarooj742@gmail.com>
2024-12-25 12:03:00 +0500

    Loading Screen added for 3 seconds and then navigate to welcome screen

--- VibeCare/App.js
+++ VibeCare/App.js
@@ -1,5 +1,6 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, Image, ActivityIndicator, Platform, StatusBar as RNStatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -14,42 +15,81 @@ import WelcomeScreen from './screens/WelcomeScreen';
 import AdminSignIn from './screens/admin';
 import admindashboard from './screens/admindashboard';
 
-
-
 const Stack = createNativeStackNavigator();
+
+// Loading Screen Component
+function LoadingScreen({ navigation }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace("WelcomeScreen"); // Always go to WelcomeScreen
+    }, 3000);
+
+    return () => clearTimeout(timer); // cleanup timer
+  }, [navigation]);
+
+  return (
+    <View style={styles.loadingContainer}>
+      <Image source={require('./assets/images/mind.png')} style={styles.image} />
+      <Text style={styles.quoteText}>
+        "A beautiful mind is a treasure trove of great ideas and dreams."
+      </Text>
+      <ActivityIndicator size="large" color="#5F0F09" style={styles.activityIndicator} />
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Stack.Navigator initialRouteName="WelcomeScreen">
+      <Stack.Navigator initialRouteName="LoadingScreen">
+        {/* Loading Screen */}
         <Stack.Screen 
-          name="SignupScreen" 
-          component={SignupScreen} 
-          options={{ headerShown: false }} // hides default header
+          name="LoadingScreen" 
+          component={LoadingScreen} 
+          options={{ headerShown: false }} 
         />
-            <Stack.Screen name="WelcomeScreen" component={WelcomeScreen}  options={{ headerShown: false }}  />
-         <Stack.Screen name="GenderSelectionScreen" component={GenderSelectionScreen}  options={{ headerShown: false }} />
-            <Stack.Screen name="AgeSelectionScreen" component={AgeSelectionScreen}  options={{ headerShown: false }} />
-            <Stack.Screen name="Relation" component={Relation}  options={{ headerShown: false }} />
-            <Stack.Screen name="LivingSituationPage" component={LivingSituationPage}  options={{ headerShown: false }}  />
-            <Stack.Screen name="SigninScreen" component={SigninScreen}  options={{ headerShown: false }}  />
-            <Stack.Screen name="ExploreScreen" component={ExploreScreen}  options={{ headerShown: false }}  />
-             <Stack.Screen name="AdminSignIn" component={AdminSignIn} options={{ headerShown: false }}/>
-             <Stack.Screen name='admindashboard' component={admindashboard} options={{ headerShown: false }}/>
-
-
-
+        
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="GenderSelectionScreen" component={GenderSelectionScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="AgeSelectionScreen" component={AgeSelectionScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Relation" component={Relation} options={{ headerShown: false }} />
+        <Stack.Screen name="LivingSituationPage" component={LivingSituationPage} options={{ headerShown: false }} />
+        <Stack.Screen name="SigninScreen" component={SigninScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ExploreScreen" component={ExploreScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="AdminSignIn" component={AdminSignIn} options={{ headerShown: false }} />
+        <Stack.Screen name="admindashboard" component={admindashboard} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loadingContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FDE6E6',
+    paddingHorizontal: 20,
+    paddingTop: Platform.OS === 'ios' ? 0 : RNStatusBar.currentHeight,
+  },
+  image: {
+    width: 350,
+    height: 350,
+    marginBottom: -30,
+    resizeMode: 'contain',
+  },
+  quoteText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#5F0F09',
+    marginBottom: 50,
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  activityIndicator: {
+    position: 'absolute',
+    bottom: 20,
   },
 });
